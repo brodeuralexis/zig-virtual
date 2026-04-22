73b1eb72303a46f0a2b0b558c4bd2778ffd34dba
Alexis Brodeur <brodeuralexis@gmail.com>
2026-04-21 21:08:39 -0400

    Support optional vtable methods
    Adds support for vtable methods marked as optional.  Such methods will
    be set to null if there is no matching declaration on the implementing
    type.

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Run the following command to add the latest tagged release to your
 `build.zig.zon` file:
 
 ``` shell
-zig fetch --save git+https://github.com/brodeuralexis/zig-virtual#v1.0.0
+zig fetch --save git+https://github.com/brodeuralexis/zig-virtual#v1.1.0
 ```
 
 You can then modify your `build.zig` to import it like so:
@@ -26,4 +26,41 @@ const virtual_dep = b.dependency("virtual", .{ target = target, .optimize = opti
 const virtual_mod = virtual_dep.module("virtual");
 
 exe.root_module.addImport("virtual", virtual_mod);
+```
+
+## Example
+
+```zig
+const Shape = struct {
+    vtable: *const VTable,
+    ptr: *anyopaque,
+
+    const VTable = struct {
+        area: *const fn(*anyopaque) f64,
+    };
+
+    fn area(shape: Shape) f64 {
+        return shape.vtable.area(shape.ptr);
+    }
+};
+
+const Circle = struct {
+    radius: f64,
+
+    fn shape(circle: *Circle) Shape {
+        return .{
+            .vtable = virtual.create(Shape.VTable, Circle, .fat_pointer),
+            .ptr = circle,
+        };
+    }
+
+    fn area(circle: *Circle) f64 {
+        return std.math.pi * circle.radius * circle.radius;
+    }
+};
+
+var circle = Circle{ .radius = 42 };
+const shape = circle.shape();
+
+try std.testing.expectEqual(5541.76944093, shape.area());
 ```
--- src/virtual.zig
+++ src/virtual.zig
@@ -18,17 +18,167 @@ pub const VTableKind = union(enum) {
 
 /// Creates a compile-time known `VTable` wrapping a type `T`, with `kind`
 /// describing the kind of virtual table we are dealing with.
+///
+/// ## Virtual Table
+///
+/// A virtual method table is a mecanism used in low-level programming languages
+/// to support dynamic dispatch. It allows a program to determine which
+/// implementation of a virtual function to call at runtime rather than at
+/// compile-time.
+///
+/// The virtual table must be a *struct* containing only constant pointers to
+/// functions:
+///
+/// ```zig
+/// const Shape = struct {
+///     ptr: *const anyopaque,
+///     vtable: *const VTable,
+///
+///     const VTable = struct {
+///         area: *const fn (*anyopaque) f64,
+///     };
+/// };
+/// ```
+///
+/// The `create` function is able to instantiate a virtual table for an
+/// implementing type, reducing the amount of boilerplate code one must write:
+///
+/// ```zig
+/// const Circle = struct {
+///     radius: f64,
+///
+///     fn area(circle: *Circle) f64 {
+///         return std.math.pi * circle.radius * circle.radius;
+///     }
+/// };
+///
+/// var circle = Circle{ .radius = 42 };
+///
+/// const shape = Shape{
+///     .ptr = &circle,
+///     .vtable = virtual.create(Shape.VTable, Circle, .fat_pointer),
+/// };
+/// ```
+///
+/// ## Name Mapping
+///
+/// In Zig, naming conventions are different for *struct* fields and function
+/// names.  `create` accounts for this by inflecting the function name in
+/// *camelCase* from the virtual table's method name in *snake_case*:
+///
+/// ```zig
+/// const VTable = struct {
+///     some_long_method_name: *const fn (*anyopaque) void,
+/// };
+///
+/// const Impl = struct {
+///     // NOTE: This maps to `VTable.some_long_method_name`.
+///     fn someLongMethodName(impl: *Impl) void { ... }
+/// };
+/// ```
+///
+/// To ensure that inflection is done correctly, we suggest using only C
+/// compatible identifiers using ASCII characters.
+///
+/// ## Optional Method
+///
+/// When using `create`, virtual table methods may be defined as optional. If
+/// the method is not defined on the implementing type, it is set to `null` on
+/// the returned virtual table instance:
+///
+/// ```zig
+/// const VTable = struct {
+///     optional_method: ?*const fn (*anyopaque) u32,
+/// };
+///
+/// const Impl = struct {};
+///
+/// const vtable = virtual.create(VTable, Impl, .fat_pointer);
+/// std.debug.assert(vtable.optional_method == null);
+/// ```
+///
+/// ## Const Correctness
+///
+/// When designing interfaces, it is oftentimes useful to be able to ensure that
+/// a virtual method cannot modify the implementation on which it operates.  The
+/// `create` function ensures that const is enforced correctly on implementing
+/// types:
+///
+/// ```zig
+/// const VTable = struct {
+///     some_constant_method: *const fn(*const anyopaque, arg: u32) anyerror!void,
+/// };
+///
+/// const Impl = struct {
+///     // NOTE: It would be a compile error to use `impl: *Impl` here.
+///     fn someConstantMethod(impl: *const Impl, arg: u32) anyerror!void {
+///         ...
+///     }
+/// };
+/// ```
+///
+/// ## Fat Pointer Interfaces
+///
+/// Fat pointer interfaces wrap both a pointer to the virtual table and a
+/// pointer to the implementation in the interface:
+///
+/// ```zig
+/// const Shape = struct {
+///     vtable: *const VTable,
+///     ptr: *anyopaque,
+///
+///     const VTable = struct {
+///         area: *const fn(shape: *anyopaque) f64,
+///     };
+/// };
+/// ```
+///
+/// The `create` function can be used to create a virtual table for such a type:
+///
+/// ```zig
+/// const Circle = struct {
+///     radius: f64,
+///
+///     fn area(circle: *Circle) f64 {
+///         return std.math.pi * circle.radius * circle.radius;
+///     }
+///
+///     fn shape(circle: *Circle) Shape {
+///         return .{ .vtable = virtual.create(Shape.VTable, Circle, .fat_pointer), .ptr = circle };
+///     }
+/// };
+/// ```
+///
+/// ## Polymorphic Interfaces
+///
+/// Polymorphic interfaces use composition to emulate inheritance, embedding the
+/// interface as a field on the implementing type:
+///
+/// ```zig
+/// const Shape = struct {
+///     vtable: *const VTable,
+///
+///     const VTable = struct {
+///         area: *const fn(shape: *Shape) f64,
+///     };
+/// };
+/// ```
+///
+/// The `create` function can also be used to create a virtual table for such a
+/// type:
+///
+/// ```zig
+/// const Circle = struct {
+///     shape: Shape = .{ .vtable = virtual.create(Shape.VTable, Circle, .{ .field_parent_ptr = "shape" }) },
+///     radius: f64,
+///
+///     fn area(circle: *Circle) f64 {
+///         return std.math.pi * circle.radius * circle.radius;
+///     }
+/// };
+/// ```
 pub fn create(comptime VTable: type, comptime T: type, comptime kind: VTableKind) *const VTable {
     const static = struct {
-        fn isConstMethod(comptime Method: type) bool {
-            const method_info = @typeInfo(Method).@"fn";
-
-            return method_info.params.len > 0 and
-                method_info.params[0].type != null and
-                @typeInfo(method_info.params[0].type.?) == .pointer and
-                @typeInfo(method_info.params[0].type.?).pointer.is_const;
-        }
-
         const transforms = switch (kind) {
             .fat_pointer => struct {
                 fn constant(ptr: *const anyopaque) *const T {
@@ -56,32 +206,41 @@ pub fn create(comptime VTable: type, comptime T: type, comptime kind: VTableKind
             var result: VTable = undefined;
 
             for (methods) |method| {
-                const method_info = @typeInfo(method.type);
+                if (MethodKind.extract(method)) |method_kind| {
+                    const Method = method_kind.toType();
+                    const function_name = inflection.toCase(.zig_function, method.name);
+                    const transform = if (method_kind.isConst()) transforms.constant else transforms.mutable;
 
-                if (method_info != .pointer or
-                    !method_info.pointer.is_const or
-                    @typeInfo(method_info.pointer.child) != .@"fn")
-                {
+                    switch (method_kind) {
+                        .required => {
+                            @field(result, method.name) = wrap(
+                                Method,
+                                @field(T, function_name),
+                                transform,
+                            );
+                        },
+                        .optional => {
+                            if (@hasDecl(T, function_name)) {
+                                @field(result, method.name) = wrap(
+                                    Method,
+                                    @field(T, function_name),
+                                    transform,
+                                );
+                            } else {
+                                @field(result, method.name) = null;
+                            }
+                        },
+                    }
+                } else {
                     @compileError(
                         std.fmt.comptimePrint(
-                            \\ A vtable must only contain function pointers, got:
-                            \\  
+                            \\ A vtable must only contain function pointers, got:                        
                             \\   {s}: {},
                         ,
                             .{ method.name, method.type },
                         ),
                     );
                 }
-
-                const Method = method_info.pointer.child;
-                const function_name = inflection.toCase(.zig_function, method.name);
-                const transform = if (isConstMethod(Method)) transforms.constant else transforms.mutable;
-
-                @field(result, method.name) = wrap(
-                    Method,
-                    @field(T, function_name),
-                    transform,
-                );
             }
 
             break :make_vtable result;
@@ -98,11 +257,19 @@ test "vtable using a fat pointer" {
 
         const VTable = struct {
             area: *const fn (*const anyopaque) f64,
+            volume: ?*const fn (*const anyopaque) f64,
             some_super_long_function_name: *const fn (*anyopaque, i32, [:0]const u8) anyerror!void,
         };
 
         fn area(shape: @This()) f64 {
             return shape.vtable.area(shape.ptr);
+        }
+
+        fn volume(shape: @This()) ?f64 {
+            return if (shape.vtable.volume) |volume_fn|
+                volume_fn(shape.ptr)
+            else
+                null;
         }
 
         fn someSuperLongFunctionName(shape: @This(), i: i32, str: [:0]const u8) anyerror!void {
@@ -142,6 +309,11 @@ test "vtable using a fat pointer" {
         std.math.sqrt(std.math.floatEps(f64)),
     );
 
+    try std.testing.expectEqual(
+        null,
+        circle.shape().volume(),
+    );
+
     try std.testing.expectError(
         error.Expected,
         circle.shape().someSuperLongFunctionName(42, "Hello"),
@@ -156,11 +328,19 @@ test "vtable using field parent" {
 
         const VTable = struct {
             area: *const fn (*const Self) f64,
+            volume: ?*const fn (*const Self) f64,
             some_super_long_function_name: *const fn (*Self, i32, [:0]const u8) anyerror!void,
         };
 
         fn area(shape: *Self) f64 {
             return shape.vtable.area(shape);
+        }
+
+        fn volume(shape: *Self) ?f64 {
+            return if (shape.vtable.volume) |volume_fn|
+                volume_fn(shape)
+            else
+                null;
         }
 
         fn someSuperLongFunctionName(shape: *Self, i: i32, str: [:0]const u8) anyerror!void {
@@ -194,11 +374,67 @@ test "vtable using field parent" {
         std.math.sqrt(std.math.floatEps(f64)),
     );
 
+    try std.testing.expectEqual(
+        null,
+        circle.shape.volume(),
+    );
+
     try std.testing.expectError(
         error.Expected,
         circle.shape.someSuperLongFunctionName(42, "Hello"),
     );
 }
+
+const MethodKind = union(enum) {
+    required: type,
+    optional: type,
+
+    fn toInfo(comptime method_kind: MethodKind) std.builtin.Type.Fn {
+        return switch (method_kind) {
+            inline else => |value| @typeInfo(value).@"fn",
+        };
+    }
+
+    fn toType(comptime method_kind: MethodKind) type {
+        return switch (method_kind) {
+            inline else => |value| value,
+        };
+    }
+
+    fn extract(comptime method: std.builtin.Type.StructField) ?MethodKind {
+        const method_info = @typeInfo(method.type);
+
+        return switch (method_info) {
+            .pointer => |pointer_info| MethodKind{ .required = doExtract(pointer_info) orelse return null },
+            .optional => |optional_info| switch (@typeInfo(optional_info.child)) {
+                .pointer => |pointer_info| MethodKind{ .optional = doExtract(pointer_info) orelse return null },
+                else => null,
+            },
+            else => null,
+        };
+    }
+
+    fn doExtract(comptime pointer_info: std.builtin.Type.Pointer) ?type {
+        if (!pointer_info.is_const) {
+            return null;
+        }
+
+        if (@typeInfo(pointer_info.child) != .@"fn") {
+            return null;
+        }
+
+        return pointer_info.child;
+    }
+
+    fn isConst(comptime method_kind: MethodKind) bool {
+        const method_info = method_kind.toInfo();
+
+        return method_info.params.len > 0 and
+            method_info.params[0].type != null and
+            @typeInfo(method_info.params[0].type.?) == .pointer and
+            @typeInfo(method_info.params[0].type.?).pointer.is_const;
+    }
+};
 
 fn wrap(comptime Method: type, comptime function: anytype, comptime transform: anytype) Method {
     const method_info = @typeInfo(Method).@"fn";
